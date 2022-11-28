5dd18b97680d3f3a4a382d92b28cd541a21b3d59
Elmir Surkee <elmirsurkeev@gmail.com>
2022-11-28 21:37:32 +0600

    learned min, max, abs, pow, round and input

--- tasks.py
+++ tasks.py
@@ -1,2 +1,13 @@
-print(5 / 3) 
+print('Start learn Python') 
 # min () передает минимальный элемент
+print("Result:", min(5, 6, -2, 1, 123, -42))
+# max () передает максимальный элемент
+print("Result:", max(5, 6, -2, 1, 123, -42))
+# abs () передает противополжное число + становится -, а - становиться +
+# pow () возводит число в степень, как **
+print('Result:', pow(5, 4));
+# round () округляет число
+print('Result:', round (5/2));
+# input() get data from user
+input("Enter your age")
+
