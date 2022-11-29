9660f391499bc22671413c52e288a84bb66ee874
Elmir Surkee <elmirsurkeev@gmail.com>
2022-11-29 21:20:50 +0600

    to learn data type in Python

--- tasks.py
+++ tasks.py
@@ -1,18 +1,32 @@
-print('Start learn Python') 
-# min () передает минимальный элемент
-print("Result:", min(5, 6, -2, 1, 123, -42))
-# max () передает максимальный элемент
-print("Result:", max(5, 6, -2, 1, 123, -42))
-# abs () передает противополжное число + становится -, а - становиться +
-# pow () возводит число в степень, как **
-print('Result:', pow(5, 4));
-# round () округляет число
-print('Result:', round (5/2));
-# input() get data from user
-input("Enter your age")
+# print('Start learn Python')
+# # min () передает минимальный элемент
+# print("Result:", min(5, 6, -2, 1, 123, -42))
+# # max () передает максимальный элемент
+# print("Result:", max(5, 6, -2, 1, 123, -42))
+# # abs () передает противополжное число + становится -, а - становиться +
+# # pow () возводит число в степень, как **
+# print('Result:', pow(5, 4));
+# # round () округляет число
+# print('Result:', round (5/2));
+# # input() get data from user
+# input("Enter your age")
 #создаем переменную. Can to change variable. Can to delete = del variable
-number = 5;
-print(number)
-digit = 4.539834
-word = 'Result ';
-print(word, digit)
+# number = 5;
+# print(number)
+# to change type data number to str can with str() function
+# to change type data srt to number can with int() function
+# digit = 4.539834
+# word = 'Result '
+# str_num = '4'
+# print(word + str(digit))
+# print(digit + int(str_num))
+#when we reaceive data from the user.
+#  Variable type STRING.
+#  Importand to remember when receiving data from input. Out helpMan int()
+
+num1 = 5
+num2 = 4
+print('Result', num1 + num2)
+print('Result', num1 - num2)
+print('Result', num1 * num2)
+print('Result', num1 / num2)
